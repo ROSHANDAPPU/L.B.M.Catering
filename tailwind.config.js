15a8d16527f18ecdd7ba132c1e5b5a962ce1b05b
Hilasmic <163965137+ROSHANDAPPU@users.noreply.github.com>
2025-10-20 14:53:14 -0500

    feat: Apply typography guidelines and configure deployment

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'libre-baskerville': ['"Libre Baskerville"', 'serif'],
+        'montserrat': ['"Montserrat"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
